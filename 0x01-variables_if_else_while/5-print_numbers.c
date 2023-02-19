191a6d41d42987923b1da227acf3eadaf5d9672f
snek111 <snek.b111@gmail.com>
2023-02-19 14:58:07 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: always 0 for success
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
