8b91717e3d11de17f592d1828c4fa2e94ac65974
snek111 <snek.b111@gmail.com>
2023-02-19 23:59:49 -0800

    inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+int c;
+int d;
+d = 0;
+while (c < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('0' + c);
+if (c + d != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
