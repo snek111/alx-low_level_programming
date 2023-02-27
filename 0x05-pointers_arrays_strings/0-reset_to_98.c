72160ee90aadde75fb8240ec6ba19854c4ae694e
snek111 <snek.b111@gmail.com>
2023-02-27 07:51:07 -0800

    battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,8 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
- * the value it points to 98
- *
+ * the value it points to to 98
  * @n: An integer input pointer
- *
  * Return: nothing
  */
 void rest_to_98(int *n)
