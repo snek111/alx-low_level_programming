2cef6bed207b8a1a7f2cb5759d358e208ecd6c47
snek111 <snek.b111@gmail.com>
2023-03-08 15:55:00 -0800

    pow-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,12 @@ int _pow_recursion(int x, int y)
 	int result = x;
 
 	if (y < 0)
-		return(-1);
+		return (-1);
 
-	else if(y == 0)
+	else if (y == 0)
 		return (1);
 
-	result*= _pow_recursion(x,y - 1);
+	result *= _pow_recursion(x, y - 1);
 
 	return (result);
 }
